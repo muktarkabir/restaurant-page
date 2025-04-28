c8dd2e5d52273c2350236a9a454e4a793096d9c5
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-04-29 00:34:40 +0100

    Dynamically render homepage content with JS.
    At the same time making the codebase modular i.e each "page" is its own
    module containing its seperate styles. The same should be done for assets
    but i kind of like having a central asset location for now.

---
+++ src/components/homepage/homepage.js
@@ -0,0 +1,34 @@
+import "./homepage.css";
+import heroImagePath from "../../assets/images/Sunny-Frantz.jpg";
+
+export default function createHomepage() {
+  const homePageContainer = document.createElement("div");
+  homePageContainer.classList.add("home-page-content");
+
+  const heroTextContainer = document.createElement("div");
+  heroTextContainer.classList.add("hero-text");
+  const headLine = document.createElement("h1");
+  const headLineSpan = document.createElement("span");
+  headLineSpan.textContent = "Eat.com";
+  headLine.appendChild(headLineSpan);
+  headLine.textContent += "Your ultimate destination for delicious food";
+  const loremText = document.createElement("p");
+  loremText.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. A natusquos ea, rerum eum dolorum qui incidunt veritatis inventore minuseligendi. Laborum, sunt voluptatem asperiores quis magnam rationeculpa laudantium?`;
+  const expLoreButton = document.createElement("button");
+  expLoreButton.type = "button";
+  expLoreButton.textContent = "Explore -->";
+
+  heroTextContainer.append(headLine, loremText, expLoreButton);
+
+  const heroImageContainer = document.createElement("div");
+  heroImageContainer.classList.add("picture");
+  const heroImage = document.createElement("img");
+  heroImage.src = heroImagePath;
+  heroImage.alt = "Picture of Food";
+
+  heroImageContainer.appendChild(heroImage);
+
+  homePageContainer.append(heroTextContainer, heroImageContainer);
+
+  return homePageContainer;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import "./style.css";
+import  homepage  from "./components/homepage/homepage.js";
+
+const content = document.querySelector("#content");
+content.append(homepage());
